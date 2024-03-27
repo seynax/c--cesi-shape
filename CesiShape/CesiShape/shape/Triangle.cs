47ba1648a56eb391bd805452cd152a12d9fb0e92
seynax <seyronax@gmail.com>
2024-03-27 10:08:50 +0100

    feature(shape): Shape interface with Area method. Triangle, Circle, Rectangle. ShapeUtils to ate multiple shapes area. Ask user to shapes area calculation in Main.

---
+++ CesiShape/CesiShape/shape/Triangle.cs
@@ -0,0 +1,26 @@
+﻿using System.Runtime.ConstrainedExecution;
+
+namespace CesiShape.Shape
+{
+    public class Triangle : Shape
+    {
+        public double Side0;
+        public double Side1;
+        public double Side2;
+
+
+        public Triangle(double side0, double side1, double side2)
+        {
+            this.Side0 = side0;
+            this.Side1 = side1;
+            this.Side2 = side2;
+        }
+
+        public double area()
+        {
+            var semiPerimeter = (this.Side0 + this.Side1 + this.Side2) / 2.0D;
+            var area = Math.Sqrt(semiPerimeter * (semiPerimeter - this.Side0) * (semiPerimeter - this.Side1) * (semiPerimeter - this.Side1) * (semiPerimeter - this.Side2));
+            return area;
+        }
+    }
+}
